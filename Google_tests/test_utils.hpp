59013f099166c550cce31c5b445cbabacef3929f
Colin Jones <colin.jones@epfl.ch>
2022-04-17 16:55:09 +0200

    Simple unit test for problem obj and constraints

--- Google_tests/test_utils.hpp
+++ Google_tests/test_utils.hpp
@@ -2,7 +2,7 @@
  * Write sparse matrix in triplet format to cout
  */
 template<typename scalar_t>
-void dump_sparse(std::string name, Eigen::SparseMatrix<scalar_t> S)
+void dump(Eigen::SparseMatrix<scalar_t> S, std::string name="ground")
 {
     std::cout << std::endl;
     std::cout << "auto " << name << " = triplet_to_sparse<" << type_name<scalar_t>() << ">(" << S.rows() << "," << S.cols() << ",";
@@ -27,4 +27,22 @@ Eigen::SparseMatrix<scalar_t> triplet_to_sparse(int rows, int cols, std::initial
     S.setFromTriplets(trip.begin(), trip.end());
     S.makeCompressed();    
     return S;
+}
+
+template<typename Derived>
+void dump(const Eigen::MatrixBase<Derived>& S, std::string name="ground", std::string type="scalar_t")
+{
+    std::cout << std::endl;
+    std::cout << "Eigen::MatrixX<" << type << "> " << name << "(" << S.rows() << "," << S.cols() << ");" << std::endl;
+    std::cout << name << " << ";
+    std::cout.precision(5);
+    bool first = true;
+    for(int c=0; c<S.cols(); c++)
+      for(int r=0; r<S.rows(); r++)
+      {
+        if(first) first = false;
+        else std::cout << ",";
+        std::cout << S(r,c);
+      }
+    std::cout << ";" << std::endl;
 }
