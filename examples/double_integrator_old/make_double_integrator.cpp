0814dc8a175600644fe65e130f315f606414def9
Roland Schwan <roland.schwan@mikrounix.com>
2022-08-24 12:15:29 +0200

    delete the old double integrator examples

--- examples/double_integrator_old/make_double_integrator.cpp
+++
@@ -1,7 +0,0 @@
-#include "double_integrator.hpp"
-
-TapeInfo<OCP> create_double_integrator(OCP& ocp)
-{
-  Problem prob = laopt::generate(ocp);
-  return prob;
-}
--- examples/double_integrator_old/make_double_integrator.hpp
+++
@@ -1,3 +0,0 @@
-#include "double_integrator.hpp"
-
-std::pair<Problem, OCP> create_double_integrator(OCP& ocp);
