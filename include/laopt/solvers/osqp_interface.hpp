9ac4c833c943c47d4ca826bfe9fa11f05739455a
Roland Schwan <roland.schwan@mikrounix.com>
2022-12-08 19:02:28 +0100

    don't switch back to reuse pattern if constraint pattern is changed

--- include/laopt/solvers/osqp_interface.hpp
+++ include/laopt/solvers/osqp_interface.hpp
@@ -182,8 +182,6 @@ private:
                         const Eigen::Ref<const Eigen::VectorX<scalar_t>>& Alb,
                         const Eigen::Ref<const Eigen::VectorX<scalar_t>>& Aub) noexcept
     {
-        bool prev_reuse_pattern = this->settings().reuse_pattern;
-
         bool constraints_type_changed = this->parse_constraints_bounds(xlb, xub, Alb, Aub);
         if (constraints_type_changed)
         {
@@ -193,8 +191,6 @@ private:
 
         construct_osqp_cost(H, f);
         construct_osqp_constraints(xlb, xub, A, Alb, Aub);
-
-        this->settings().reuse_pattern = prev_reuse_pattern;
     }
 
     EIGEN_STRONG_INLINE void
