5bad8b35677ec9fcb98e1826955fc0b0df025f79
Roland Schwan <roland.schwan@mikrounix.com>
2026-05-16 18:01:06 +0200

    clean up cmake

--- cmake/FindIpopt.cmake
+++ cmake/FindIpopt.cmake
@@ -1,28 +1,46 @@
 find_package(PkgConfig QUIET)
+if (PkgConfig_FOUND)
+    pkg_check_modules(PC_Ipopt QUIET ipopt)
+endif()
 
-# Check to see if pkgconfig is installed.
-pkg_check_modules(PC_Ipopt ipopt)
-
-# Definitions
 set(Ipopt_DEFINITIONS ${PC_Ipopt_CFLAGS_OTHER})
 
-# Include directories
 find_path(Ipopt_INCLUDE_DIRS
     NAMES IpIpoptNLP.hpp
-    HINTS ${PC_Ipopt_INCLUDEDIR}
-    PATHS "${CMAKE_INSTALL_PREFIX}/include")
+    HINTS
+        ${PC_Ipopt_INCLUDEDIR}
+        ${PC_Ipopt_INCLUDE_DIRS}
+    PATHS
+        "${CMAKE_INSTALL_PREFIX}/include"
+)
 
-# Libraries
 find_library(Ipopt_LIBRARIES
     NAMES ipopt
-    HINTS ${PC_Ipopt_LIBDIR})
+    HINTS
+        ${PC_Ipopt_LIBDIR}
+        ${PC_Ipopt_LIBRARY_DIRS}
+)
 
-# Version
-set(Ipopt_VERSION ${PC_IPOPT_VERSION})
+set(Ipopt_VERSION ${PC_Ipopt_VERSION})
 
-# Set (NAME)_FOUND if all the variables and the version are satisfied.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Ipopt
-    FAIL_MESSAGE  DEFAULT_MSG
     REQUIRED_VARS Ipopt_INCLUDE_DIRS Ipopt_LIBRARIES
-    VERSION_VAR   Ipopt_VERSION)
+    VERSION_VAR Ipopt_VERSION
+)
+
+if (Ipopt_FOUND AND NOT TARGET Ipopt::Ipopt)
+    add_library(Ipopt::Ipopt UNKNOWN IMPORTED)
+    set_target_properties(Ipopt::Ipopt PROPERTIES
+        IMPORTED_LOCATION "${Ipopt_LIBRARIES}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Ipopt_INCLUDE_DIRS}"
+    )
+    if (Ipopt_DEFINITIONS)
+        set_property(TARGET Ipopt::Ipopt PROPERTY INTERFACE_COMPILE_OPTIONS "${Ipopt_DEFINITIONS}")
+    endif()
+    if (PC_Ipopt_LDFLAGS_OTHER)
+        set_property(TARGET Ipopt::Ipopt PROPERTY INTERFACE_LINK_OPTIONS "${PC_Ipopt_LDFLAGS_OTHER}")
+    endif()
+endif()
+
+mark_as_advanced(Ipopt_INCLUDE_DIRS Ipopt_LIBRARIES)
